b7f7bcac921493d99b83498fd762b2ac9768a88a
prasanthoyasco <prasanthoyasco@gmai.com>
2025-12-01 17:57:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import LoginPage from './Component/LoginPage'
+import Inventorylogin from './Component/inventorylogin'
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 900,
+      once: true,
+      easing: "ease-out",
+    });
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        {/* Login Page */}
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Inventory Login */}
+        <Route path="/inventory-login" element={<Inventorylogin />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Component/LoginPage.jsx
@@ -0,0 +1,125 @@
+import { Mail, Lock, EyeClosed, Eye } from "lucide-react";
+import loginIllustration from "/hms.svg";
+import { useState } from "react";
+
+export default function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <div className="min-h-screen w-full flex">
+      {/* LEFT SECTION */}
+      <div
+        data-aos="fade-right"
+        className="hidden lg:flex w-2/3 items-center justify-center bg-white/80 p-10"
+      >
+        <img
+          src={loginIllustration}
+          alt="Medical Illustration"
+          className="w-[85%] max-w-[650px] object-contain"
+        />
+      </div>
+
+      {/* RIGHT SECTION — slide-right */}
+      <div
+        data-aos="fade-left"
+        className="flex w-full lg:w-1/3 items-center justify-center px-6 lg:px-16 py-10 bg-white shadow-[-10px_0_20px_rgba(0,0,0,0.05)]"
+      >
+        <div
+          className="w-full max-w-md"
+          data-aos="zoom-in"
+          data-aos-delay="200"
+        >
+          {/* Logo */}
+          <div className="flex justify-center mb-8">
+            <img src="/hms_logo.svg" alt="logo" className="h-24" />
+          </div>
+
+          {/* Title */}
+          <h2 data-aos="fade-up" className="text-center text-xl font-bold mb-8">
+            Login into your account
+          </h2>
+
+          {/* Form */}
+          <form className="space-y-6">
+            {/* Email */}
+            <div data-aos="fade-up" data-aos-delay="100">
+              <label className="text-sm font-medium">Email Address</label>
+              <div className="relative mt-1">
+                <input
+                  type="email"
+                  placeholder="alex@email.com"
+                  className="w-full bg-gray-100 rounded-lg border border-gray-300 px-4 py-2 pr-12 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <div className="bg-blue-600 h-full aspect-square rounded-md absolute right-0 top-0 flex items-center justify-center">
+                  <Mail
+                    className="absolute right-4 top-3.5 text-gray-100"
+                    size={20}
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Password */}
+            <div data-aos="fade-up" data-aos-delay="200">
+              <label className="text-sm font-medium">Password</label>
+              <div className="relative mt-1">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Enter your password"
+                  className="w-full rounded-lg bg-gray-100 border border-gray-300 px-4 py-2 pr-20 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+
+                {/* Eye Icon (left inside the input) */}
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-13 top-1/2 -translate-y-1/2 text-[#1E2772] hover:text-[#1E2772]/80 transition-padding px-3"
+                >
+                  {showPassword ? <EyeClosed size={20} /> : <Eye size={20} />}
+                </button>
+
+                {/* Blue Lock Box (unchanged) */}
+                <div className="bg-blue-600 h-full aspect-square rounded-md absolute right-0 top-0 flex items-center justify-center">
+                  <Lock className="text-white" size={20} />
+                </div>
+              </div>
+
+              <div className="text-right mt-1">
+                <a href="#" className="text-xs text-[#1E2772] hover:underline">
+                  Forgot Password?
+                </a>
+              </div>
+            </div>
+
+            {/* Login Button */}
+            <button
+              data-aos="fade-up"
+              data-aos-delay="300"
+              type="submit"
+              className="w-full py-3 rounded-lg bg-linear-to-r from-[#003893] to-[#005FF9] text-white font-semibold  shadow-lg shadow-blue-200 transition-all duration-500 ease-in-out hover:bg-linear-to-l" >
+              Login Now
+            </button>
+
+            {/* Divider */}
+            <div
+              className="flex items-center gap-3"
+              data-aos="fade-up"
+              data-aos-delay="350"
+            >
+              <div className="grow border-t border-gray-300" />
+              <span className="text-xs text-gray-600 font-bold">OR</span>
+              <div className="grow border-t border-gray-300" />
+            </div>
+
+            {/* Signup Button */}
+            <button
+              type="button"
+              className="w-full py-3 rounded-lg border border-blue-600 text-blue-600 font-semibold hover:bg-blue-50 transition"
+            >
+              Signup Now
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
